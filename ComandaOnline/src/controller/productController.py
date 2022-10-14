2d86e0e3fa7bfab0a1b318beef593b37dd23f0b5
Adrian Camus <adrianedgardocamus@gmail.com>
2022-10-13 23:35:56 -0300

    Add Structure MVC
    The project structure is added following the controller view model.

---
+++ ComandaOnline/src/controller/productController.py
@@ -0,0 +1,6 @@
+#addProduct() - crea un nuevo producto
+#updateProduct() - actualiza un producto
+#deleteProduct() - elimina un producto
+#getProduct() - obtiene un producto por id
+#getAllProducts() - obtiene todos los productos
+#searchProduct() - busca un producto por nombre
