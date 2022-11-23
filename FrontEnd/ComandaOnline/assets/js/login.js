fd59432d95c6f752214cc93d7cbf22a97c781dc3
Adrian Camus <adrianedgardocamus@gmail.com>
2022-11-22 22:30:48 -0300

    Update login.js

--- FrontEnd/ComandaOnline/assets/js/login.js
+++ FrontEnd/ComandaOnline/assets/js/login.js
@@ -1,38 +1,48 @@
 const myForm = document.getElementById('formulario');
 const urlPost = 'http://localhost:8000/api/auth/login/';
 
-myForm.addEventListener('submit', function (e){
+myForm.addEventListener('submit', function (e) {
     e.preventDefault();
-    
+
     var name = document.querySelector("#inputUsername");
     var pass = document.querySelector("#inputPassword");
 
-    
-    fetch(urlPost,{
-        headers: {
-            'Accept': 'application/json, text/plain, */*',
-            'Content-Type': 'application/json'
-        },
-        method: 'POST',
 
-        body: JSON.stringify({username: name.value, password: pass.value})
+    fetch(urlPost, {
+            headers: {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-Type': 'application/json'
+            },
+            method: 'POST',
+
+            body: JSON.stringify({
+                username: name.value,
+                password: pass.value
+            })
         }).then(res => res.json()).then(data => {
+
+            console.log(data)
             
-            if(!data.key == ''){
-                error(data);
+            if (!data.key == null) {
                 $("#formulario>").empty() //empty
-                console.log(data)
-        }else{
-            console.log(data.key)
-        }
+                console.log(data.key)
+                let token = data.key
+                localStorage.setItem('token', token)
+                //window.location.href = "./dashboard.html"
+            } else if(!data.password == ''){
+                error('Contraseña: ' + data.password);
+            }
         })
-    .catch(err => console.log(err));
+        .catch(err => console.log(err));
 });
 
 
-function error(data){
+function error(text) {
+    console.log("estoy en error con " + text)
     document.getElementById("error").style.backgroundColor = "indianred";
-    document.getElementById("error").style.borderRadius = "15px";
+    document.getElementById("error").style.color = "white";
+    document.getElementById("error").style.borderRadius = "10px";
+    document.getElementById("error").style.marginTop = "10px";
     document.getElementById("error").style.textAlign = "center";
-    document.getElementById("error").innerText = data
+    document.getElementById("error").innerText = text;
 }
