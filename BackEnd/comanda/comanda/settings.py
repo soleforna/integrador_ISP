933e715c4317f4ef1b5f2a8329c955fe6d1bacd5
Adrian Camus <adrianedgardocamus@gmail.com>
2022-11-23 21:31:07 -0300

    Update settings.py

--- BackEnd/comanda/comanda/settings.py
+++ BackEnd/comanda/comanda/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+#CORS 
 CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
 CORS_ALLOW_CREDENTIALS = True
 
